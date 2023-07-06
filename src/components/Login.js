f581ff1279e7ac18549dfe9de211113f00fe745a
Egor Kirin <egorcom27@gmail.com>
2023-07-07 00:03:45 +0300

    добавлена валидация авторизации

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,41 +9,59 @@ const Login = observer(() => {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [emailValid, setEmailValid] = useState(true);
+  const [passwordValid, setPasswordValid] = useState(true);
 
-  const click = () => {
-    user.setIsAuth(true);
-    navigate(HOME_ROUTE);
+  const login = (e) => {
+    e.preventDefault();
+
+    if (email.trim() === "") {
+      setEmailValid(false);
+    }
+    if (password === "") {
+      setPasswordValid(false);
+    }
+    if (email !== "" && password !== "") {
+      user.setIsAuth(true);
+      navigate(HOME_ROUTE);
+    }
   };
 
   return (
     <div className="auth__container">
-      <form className="auth__form">
+      <form className="auth__form" onSubmit={login}>
         <div className="auth__group">
-          <label className="auth__label" for="email">
+          <label className="auth__label" htmlFor="email">
             Почта
           </label>
           <input
-            className="auth__input"
+            className={`auth__input ${!emailValid && "is-invalid"}`}
             type="email"
             placeholder="Почта"
             id="email"
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={(e) => {
+              setEmail(e.target.value);
+              setEmailValid(true);
+            }}
           />
         </div>
         <div className="auth__group">
-          <label className="auth__label" for="password">
+          <label className="auth__label" htmlFor="password">
             Пароль
           </label>
           <input
-            className="auth__input"
+            className={`auth__input ${!passwordValid && "is-invalid"}`}
             type="password"
             placeholder="Пароль"
             id="password"
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => {
+              setPassword(e.target.value);
+              setPasswordValid(true);
+            }}
           />
         </div>
         <div className="auth__btn-wrapper">
-          <button onClick={click} type="submit" className="login-button btn">
+          <button onClick={login} type="submit" className="login-button btn">
             Войти
           </button>
         </div>
